bb65e61e47295a6c9236bef85d91049db3744842
Yehya <yehyaelzaeem7@gmail.com>
2025-05-04 12:51:33 +0300

    first commit

---
+++ build/rive_common/intermediates/cxx/Debug/3a4v4m1y/logs/x86_64/build_command_rive_text.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Yehya\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Yehya\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\rive_common-0.4.15\\android\\.cxx\\Debug\\3a4v4m1y\\x86_64" ^
+  rive_text
